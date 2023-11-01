43d1d0beb9d81c721346a015356737330c0b3abd
xias984 <xias984@gmail.com>
2023-11-01 16:22:36 +0100

    import tables

--- tables.sql
+++ tables.sql
@@ -64,8 +64,62 @@ CREATE TABLE `configuration` (
 
 LOCK TABLES `configuration` WRITE;
 /*!40000 ALTER TABLE `configuration` DISABLE KEYS */;
-INSERT INTO `configuration` VALUES (1,'Home Manager','Gestionale domestico',0,'./assets/images/logo_homemanager.png',1);
+INSERT INTO `configuration` VALUES (1,'Home Manager','Gestionale domestico',0,'./assets/images/logo_homemanager.png',9);
 /*!40000 ALTER TABLE `configuration` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `finance`
+--
+
+DROP TABLE IF EXISTS `finance`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `finance` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `type` enum('E','U') NOT NULL,
+  `amount` int NOT NULL,
+  `userid` int NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `categoryid` int NOT NULL,
+  `paymentdate` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `finance`
+--
+
+LOCK TABLES `finance` WRITE;
+/*!40000 ALTER TABLE `finance` DISABLE KEYS */;
+/*!40000 ALTER TABLE `finance` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `finance_category`
+--
+
+DROP TABLE IF EXISTS `finance_category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `finance_category` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `category` varchar(60) DEFAULT NULL,
+  `iduser` int DEFAULT NULL,
+  `datainserimento` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `finance_category`
+--
+
+LOCK TABLES `finance_category` WRITE;
+/*!40000 ALTER TABLE `finance_category` DISABLE KEYS */;
+INSERT INTO `finance_category` VALUES (1,'Famiglia',33,'2023-10-29 23:28:39'),(2,'Auto',33,'2023-10-30 00:28:52'),(3,'Bollette',33,'2023-10-30 09:28:49'),(4,'Varie',33,'2023-10-30 11:54:19'),(5,'Stipendio',33,'2023-10-30 12:08:51');
+/*!40000 ALTER TABLE `finance_category` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -85,7 +139,6 @@ CREATE TABLE `social` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
 
 --
 -- Table structure for table `users`
@@ -111,7 +164,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Daniel','Costarelli','xias984@gmail.com',1,'$2y$10$8jFJwKsL28jOlHeCYsm.uuFiAfrMWfXlQl769/K/5J8ABv9WAcfjS');
+INSERT INTO `users` VALUES (1,'Daniel','Costarelli','xias984@gmail.com',1,'$2y$10$T0yGeVFWwpo/1HwHXTY3suFPiy7gqyTyhNogukzGnyKBzAVk7p6bS'),(3,'Martina','Costarelli','martinagaiac@icloud.com',0,'$2y$10$69TASCTOwuDg6j52GjVCDuXjJIIUc9OSIm7g87haStEhkkTT0Ut7.'),(4,'Leonardo','Costarelli','lcostarelli@icloud.com',0,'$2y$10$AF/lwL1WsVrnRRppQ8Q3m.PUOamsZ.k46Fkk50XYc9.NUmQwbn1T6'),(2,'Bernadette','Giordano','bernadettem.giordano@gmail.com',1,'$2y$10$UhK9oicb39k8HfJVc9hvDeGP6e..VZqY8/28Dc04oJBDhxMpwzTT.');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -124,4 +177,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-10-27 10:21:53
+-- Dump completed on 2023-11-01 16:20:29
